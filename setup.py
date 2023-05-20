7bfc956cd1be3372869e2d1042b4ecf515fbf473
Ahmed Ibrahim <leondaz@macbook.local>
2023-05-20 18:35:14 +0300

    add pre-commit & setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="paymobpy",
+    version="1.0.0",
+    description="A client library for paymob API",
+    author="LeOndaz",
+    author_email="ahmeddark369@gmail.com",
+    url="https://github.com/LeOndaz/paymobpy",
+    packages=["paymobpy"],
+)
